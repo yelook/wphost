818d93466a0091e6e829b8c7cc6a7085e2b9fd0f
yelook <admin@yelook.com>
2015-03-02 10:47:25 +0800

    fix a little bit
    i just have forgoten fix em

--- tag.php
+++ tag.php
@@ -7,7 +7,7 @@
     <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
     <div class="box">
-		<div class="pic"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img src="<?php post_thumbnail_src() ?>"/></a></div>
+		<div class="pic"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img src="<?php post_thumbnail_bigsrc() ?>"/></a></div>
 		<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
       <div class="preview"> <?php echo mb_strimwidth(strip_tags(apply_filters('the_content', $post->post_content)), 0, 260,"……"); ?> </div>
       <div class="preem"> <?php the_time('y/m/d'); ?> | <?php comments_popup_link('暂无评论', '1 条评论', '% 条评论'); ?> | <?php post_views('', ''); ?>阅读
